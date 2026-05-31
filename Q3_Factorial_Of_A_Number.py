16af6ef1ddc9ed725b5072059c7f8b32d6212fb6
gatikjuneja47-bit <gatikjuneja@gmail.com>
2026-05-31 20:03:04 +0530

    Delete Q3_Factorial_Of_A_Number.py

--- Q3_Factorial_Of_A_Number.py
+++
@@ -1,8 +0,0 @@
-print("Factorial of a Number"
-      "\nEnter a number: ", end=""
-      )
-num = int(input())
-factorial = 1
-for i in range(1, num + 1):
-    factorial *= i
-print(f"Factorial of {num} is {factorial}")
