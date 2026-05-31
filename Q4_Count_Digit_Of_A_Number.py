99df09684ebd0d5c86ffce36eacbe2de1fceebcc
gatikjuneja47-bit <gatikjuneja@gmail.com>
2026-05-31 20:03:21 +0530

    Delete Q4_Count_Digit_Of_A_Number.py

--- Q4_Count_Digit_Of_A_Number.py
+++
@@ -1,9 +0,0 @@
-print("Count of Digits in a Number"
-      "\nEnter a number: ", end=""
-      )
-num = int(input())
-count = 0
-while num != 0:
-    num //= 10
-    count += 1
-print(f"Count of digits in the number is {count}")
