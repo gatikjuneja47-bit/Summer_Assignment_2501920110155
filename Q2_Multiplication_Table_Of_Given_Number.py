e143f9778f48107c9931251d34d9b479ae83e882
gatikjuneja47-bit <gatikjuneja@gmail.com>
2026-05-31 20:02:49 +0530

    Delete Q2_Multiplication_Table_Of_Given_Number.py

--- Q2_Multiplication_Table_Of_Given_Number.py
+++
@@ -1,8 +0,0 @@
-print("Multiplication Table of a Given Number"
-      "\n------------------------------"
-      "\nEnter a number: ", end=""
-      )
-num = int(input())
-print(f"Multiplication Table of {num}")
-for i in range(1, 11):
-    print(f"{num} x {i} = {num * i}")
