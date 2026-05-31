2c48c4f7794839c6688448e188c24d93a9fd2bf6
Gatik Juneja <gatikjuneja@gmail.com>
2026-06-01 00:13:11 +0530

    Delete Day-10 directory

--- Day-10/Q1_Star_Pyramid.py
+++
@@ -1,8 +0,0 @@
-print("Star Pyramid Pattern")
-n = int(input("Enter the number of rows: "))
-for i in range(1, n + 1):
-    for space in range(n - i):
-        print(" ", end="")
-    for j in range(1, 2 * i):
-        print("*", end="")
-    print()
