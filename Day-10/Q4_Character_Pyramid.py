45a01093a671be35438487b30662a897dd041d8b
Gatik Juneja <gatikjuneja@gmail.com>
2026-06-01 00:20:23 +0530

    Add files via upload

---
+++ Day-10/Q4_Character_Pyramid.py
@@ -0,0 +1,10 @@
+print("Character Pyramid Pattern")
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    for space in range(n - i):
+        print(" ", end="")
+    for j in range(1, i + 1):
+        print(chr(64 + j), end="")
+    for j in range(i - 1, 0, -1):
+        print(chr(64 + j), end="")
+    print()
