da116b0b7170e0cdc6d3238b0c8c46b8139b834d
Gatik Juneja <gatikjuneja@gmail.com>
2026-06-23 21:45:27 +0530

    Add files via upload

---
+++ Day-24/Q3_Find_Longest_Word.py
@@ -0,0 +1,16 @@
+print("Longest Word in a String")
+string = input("Enter the string: ")
+words = 1
+for char in string:
+    if char == " ":
+        words += 1
+longest_word = ""
+current_word = ""
+for char in string:
+    if char != " ":
+        current_word += char
+    else:
+        if len(current_word) > len(longest_word):
+            longest_word = current_word
+        current_word = "" 
+print(f"Longest word in the string: {longest_word}")
