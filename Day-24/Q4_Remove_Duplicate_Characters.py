b363ab86727814df0a3fcec0a3035ffaba4cda74
Gatik Juneja <gatikjuneja@gmail.com>
2026-06-23 21:52:14 +0530

    Delete Day-24/Q4_Remove_Duplicate_Characters.py

--- Day-24/Q4_Remove_Duplicate_Characters.py
+++
@@ -1,7 +0,0 @@
-print("Removing Duplicate Characters in a String")
-string = input("Enter the string: ")
-result = ""
-for char in string:
-    if char not in result:
-        result += char
-print(f"String after removing duplicates: {result}")
